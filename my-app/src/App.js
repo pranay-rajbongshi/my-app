a40b51e0df33b9a1ddcb84dbb418b6e83736f6bc
pranay-rajbongshi <pranay@naahor.com>
2018-12-14 14:56:42 +0530

    counter added to app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount(){
+    this.increment()
+  }
+
+  increment = () => {
+    setInterval(
+      () => {
+        if(this.state.count < 10)
+          return this.setState({  count: this.state.count + 1 })
+        else
+          return this.setState({  count: 0 })
+      },
+      1000
+    )
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{this.state.count}</h1>
         </header>
       </div>
     );
